1db57b14b6224c037842ea95fb7fc81375d97372
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2023-05-10 13:53:41 +0200

    Edit : ajout swagger et graphql

--- serveur.js
+++ serveur.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import session from 'express-session';
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'graphql';
-import { Sequelize } from 'sequelize-typescript';
-import passport from 'passport';
-import { Strategy as OAuth2Strategy } from 'passport-oauth2';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json' assert { type: 'json' };
+import express from "express";
+import session from "express-session";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+import { Sequelize } from "sequelize-typescript";
+import passport from "passport";
+import { Strategy as OAuth2Strategy } from "passport-oauth2";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
+import crypto from "crypto";
 
 const app = express();
 
@@ -30,14 +31,16 @@ import driversAPIRoutes from "./routes/drivers.js"; //Création API REST (JSON)
 
 // Configure Sequelize ORM
 const sequelize = new Sequelize({
-  database: 'f1_bdd',
-  username: 'root',
-  password: 'rootpwd',
-  dialect: 'mysql',
+  database: "F1_bdd",
+  username: "root",
+  password: "rootpwd",
+  dialect: "mariadb",
+  port: 4000,
+  host: "localhost"
 });
 
 // Define your Sequelize models
-const Driver = sequelize.define('Driver', {
+const Pilotes = sequelize.define("Pilotes", {
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
@@ -50,17 +53,35 @@ const Driver = sequelize.define('Driver', {
   numero_pilotes: Sequelize.INTEGER,
 });
 
+const secret = crypto.randomBytes(32).toString("hex");
+console.log(secret); // logs a random string of 64 characters
+
+passport.serializeUser((user, done) => {
+  done(null, JSON.stringify(user));
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, JSON.parse(user));
+});
+
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Configure OAuth2 authentication
 passport.use(
-  'oauth2',
+  "github",
   new OAuth2Strategy(
     {
-      authorizationURL: 'https://oauth.provider.com/auth',
-      tokenURL: 'https://oauth.provider.com/token',
-      clientID: 'your_client_id',
-      clientSecret: 'your_client_secret',
-      callbackURL: 'http://localhost:8080/auth/callback',
+      authorizationURL: "https://github.com/login/oauth/authorize",
+      tokenURL: "https://github.com/login/oauth/access_token",
+      clientID: "095d2557e43a403a39e3",
+      clientSecret: "2c5b581cf8c9263e2c35d113a186040094827115",
+      callbackURL: "http://localhost:8080/auth/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       // Handle user authentication and authorization
@@ -71,12 +92,35 @@ passport.use(
   )
 );
 
+// Github OAuth2 authentication route
+app.use("/auth/github", passport.authenticate("github"));
+
+// Github OAuth2 callback route
+app.use(
+  "/auth/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  (req, res) => {
+    // Redirect to the home page after successful authentication
+    res.redirect("/graphql");
+  }
+);
+
+// Example protected route
+app.use(
+  "/protected",
+  passport.authenticate("github", { session: false }),
+  (req, res) => {
+    // Return data only if the user is authenticated
+    res.json({ message: "This is a protected route" });
+  }
+);
+
 // Initialize Passport and session middleware
 app.use(passport.initialize());
 
 // Définition du schéma GraphQL
 const schema = buildSchema(`
-  type Driver {
+  type Pilotes {
     id: Int!
     prenom: String!
     nom: String!
@@ -86,26 +130,32 @@ const schema = buildSchema(`
   }
 
   type Query {
-    drivers: [Driver]
+    pilotes: [Pilotes]
   }
 `);
 
-
 // Implémentation de la résolution de la requête
 const root = {
-  drivers: async () => {
-    const drivers = await Driver.findAll({
-      attributes: ['id', 'nom', 'prenom', 'nationalite', 'date_naissance', 'numero'],
+  pilotes: async () => {
+    const pilotes = await Pilotes.findAll({
+      attributes: [
+        "id",
+        "prenom",
+        "nom",
+        "nationalite",
+        "date_de_naissance",
+        "numero_pilotes",
+      ],
     });
-    return drivers;
+    return pilotes;
   },
 };
 
 
+
 // Configuration de l'API GraphQL
 app.use(
-  '/graphql',
-  passport.authenticate('oauth2', { session: false }),
+  "/graphql",
   graphqlHTTP({
     schema: schema,
     rootValue: root,
@@ -114,18 +164,11 @@ app.use(
 );
 
 // Swagger-UI endpoint
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app
   .use(express.urlencoded({ extended: true }))
   .use(express.static("public"))
-  .use(
-    session({
-      secret: "ma_clef_secrete",
-      resave: false,
-      saveUninitialized: true,
-    })
-  )
   .use("/", accueilRoutes)
   .use("/telecharger", telechargerRoutes)
   .use("/select", selectRoutes)
@@ -142,4 +185,3 @@ const port = 8080;
 httpServer.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
-
